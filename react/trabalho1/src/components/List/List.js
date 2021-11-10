ec61157eb290a7493b4276db4795dd79ba935178
ThiagoMarchi <thiagomarch@hotmail.com>
2021-11-10 13:03:48 -0300

    trabalho ok

--- react/trabalho1/src/components/List/List.js
+++ react/trabalho1/src/components/List/List.js
@@ -3,7 +3,7 @@ import styles from './List.module.css';
 function Menu ({itens,localDosItens}){
 
   return(
-    <div className={localDosItens === 'teste' ? styles.teste : styles.teste1}>
+    <div className={localDosItens === '' ? styles.teste : styles.teste1}>
       {itens.map(teste => {
         return(
         <a href={teste.href}>{teste.nome}</a>
