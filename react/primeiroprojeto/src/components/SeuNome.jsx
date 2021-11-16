def7d9c3efefda35e73275c2de3ced6306578983
ThiagoMarchi <thiagomarch@hotmail.com>
2021-11-16 01:32:40 -0300

    primeiras aulas

--- react/primeiroprojeto/src/components/SeuNome.jsx
+++ react/primeiroprojeto/src/components/SeuNome.jsx
@@ -2,7 +2,7 @@ function SeuNome({setNome}) {
   return (
     <div>
       <p>Digite seu nome</p>
-      <input type="text" placeholder="Qual é o seu nome?" onChange={(e) => setNome(e.target.value)} />
+      <input type="text" placeholder="Qual é o seu nomeeee?" onChange={(e) => setNome(e.target.value)} />
     </div>
   );
 }
